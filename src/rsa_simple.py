191c43824b6a516a0505f2550151a9f4a41c2539
Long Do <long.do1@student.unsw.edu.au>
2025-07-05 21:21:17 +1000

    refactoring code

--- src/rsa_simple.py
+++ src/rsa_simple.py
@@ -2,87 +2,87 @@ import random
 import math
 
 
-d = None
-n = None
-e = None
+class RSA_SIMPLE:
+    def __init__(self):
+        self.d = None
+        self.n = None
+        self.e = None
 
-
-
-def is_prime(number):
-    """
-    Function to check if a number is prime
-    Input: integer number
-    Output: True if is a prime number, false otherwise
-    """
-    if number < 2:
-        return False
-    if number in (2, 3):
-        return True
-    if number % 2 == 0:
-        return False
-    for i in range(3, int(math.sqrt(number)) + 1, 2):
-        if number % i == 0:
+    def is_prime(self, number):
+        if number < 2:
             return False
-    return True
+        if number in (2, 3):
+            return True
+        if number % 2 == 0:
+            return False
+        for i in range(3, int(math.sqrt(number)) + 1, 2):
+            if number % i == 0:
+                return False
+        return True
 
-
-def generate_prime (min_value, max_value):
-    """
-    Function to generate a prime number (not that efficient)
-    """
-    prime = random.randint (min_value, max_value)
-    while not is_prime(prime):
+    def generate_prime(self, min_value, max_value):
         prime = random.randint(min_value, max_value)
-    return prime
+        while not self.is_prime(prime):
+            prime = random.randint(min_value, max_value)
+        return prime
 
+    def mod_inverse(self, e, phi):
+        for d in range(3, phi):
+            if (d * e) % phi == 1:
+                return d
+        raise ValueError("Mod_inverse does not exist!")
 
-def mod_inverse(e, phi):
-    """
-    Function to find d (private key)
-    """
-    for d in range (3, phi):
-        if (d * e) % phi == 1:
-            return d 
-    raise ValueError ("Mod_inverse does not exist!")
+    def generate_keys(self):
+        while True:
+            p = self.generate_prime(1000, 50000)
+            q = self.generate_prime(1000, 50000)
+            if p == q:
+                continue
+            self.n = p * q
+            if self.n < 65536:
+                continue
+            phi_n = (p - 1) * (q - 1)
+            self.e = random.randint(3, phi_n - 1)
+            while math.gcd(self.e, phi_n) != 1 or not self.is_prime(self.e):
+                self.e = random.randint(3, phi_n - 1)
+            self.d = self.mod_inverse(self.e, phi_n)
+            break
 
-def generate_keys():
-    """Generate RSA key pair and store them in global variables."""
-    global d, n, e
-    while True:
-        p = generate_prime(1000, 50000)
-        q = generate_prime(1000, 50000)
-        if p == q:
-            continue
-        n = p * q
-        if n < 65536:
-            continue  # ensure modulus is large enough for Unicode
-        phi_n = (p - 1) * (q - 1)
-        e = random.randint(3, phi_n - 1)
-        while math.gcd(e, phi_n) != 1 or not is_prime(e):
-            e = random.randint(3, phi_n - 1)
-        d = mod_inverse(e, phi_n)
-        break
+    def get_public_key(self):
+        return (self.e, self.n)
 
-def encrypt(message):
-    """Encrypt a string message using the public key."""
-    message_bytes = message.encode('utf-8')
-    ciphertext = [pow(byte, e, n) for byte in message_bytes]
-    return ciphertext
+    def get_private_key(self):
+        return (self.d, self.n)
 
-def decrypt(ciphertext):
-    """Decrypt a list of integers using the private key."""
-    decrypted_bytes = bytes([pow(byte, d, n) for byte in ciphertext])
-    return decrypted_bytes.decode('utf-8')
+    def encrypt(self, message, public_key=None):
+        if public_key is not None:
+            e, n = public_key
+        else:
+            e, n = self.e, self.n
+
+        message_bytes = message.encode('utf-8')
+        ciphertext = [pow(byte, e, n) for byte in message_bytes]
+        return ciphertext
+
+    def decrypt(self, ciphertext, private_key=None):
+        if private_key is not None:
+            d, n = private_key
+        else:
+            d, n = self.d, self.n
+
+        decrypted_bytes = bytes([pow(byte, d, n) for byte in ciphertext])
+        return decrypted_bytes.decode('utf-8')
 
 
 if __name__ == "__main__":
-    generate_keys()
+    rsa = RSA_SIMPLE()
+    rsa.generate_keys()
 
-    message = input("Enter you message to encrypt: ")
+    message = input("Enter your message to encrypt: ")
     print("Original Message:", message)
 
-    cipher = encrypt(message)
+    cipher = rsa.encrypt(message)
     print("Encrypted:", cipher)
 
-    plain = decrypt(cipher)
+    plain = rsa.decrypt(cipher)
     print("Decrypted:", plain)
